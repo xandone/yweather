7a78dd5bb0e336e564b977592833baa69e401c5a
xandone <765478955@qq.com>
2017-07-20 16:14:51 +0800

    scroll

--- app/src/main/java/app/xandone/com/yweather/widget/GlideListView.java
+++ app/src/main/java/app/xandone/com/yweather/widget/GlideListView.java
@@ -37,6 +37,7 @@ public class GlideListView extends RelativeLayout {
 
     public void yScroll(int duration) {
         mScroller.startScroll(0, Utils.getWH()[1], 0, -Utils.getWH()[1], duration);
+        invalidate();
     }
 
     @Override
